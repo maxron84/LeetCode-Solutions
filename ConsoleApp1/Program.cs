ea091ff5ee61ddad70626083d9dad4caf39a901e
max ron <spirit@maxrax.de>
2022-10-11 13:59:33 +0200

    Added palindrome solution, minor changes to existing stuff.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main()
         {
-            LeetCode_Solutions._13._Roman_to_Integer.Solution.GetIntegerFromRoman();
+            LeetCode_Solutions
+                ._13._Roman_to_Integer
+                .Solution
+                .GetIntegerFromRoman();
+            LeetCode_Solutions
+                ._9._Palindrome_Number
+                .Solution
+                .IsPalindromeNumber();
+
+            Environment.Exit(0);
         }
     }
 }
