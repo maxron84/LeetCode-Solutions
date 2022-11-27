35976f8c2ce51a75930a0377a141bcd8d0387353
max ron <spirit@maxrax.de>
2022-11-27 12:41:44 +0100

    Changed to .NET 6 Style

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,21 @@
-﻿namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main()
-        {
-            LeetCode_Solutions
-                ._13._Roman_to_Integer
-                .Solution
-                .GetIntegerFromRoman();
-            LeetCode_Solutions
-                ._9._Palindrome_Number
-                .Solution
-                .IsPalindromeNumber();
+﻿int[] twoSumInputElements = { 2, 7, 11, 15 };
+int twoSumTarget = 9;
 
-            Environment.Exit(0);
-        }
-    }
-}
+LeetCode_Solutions
+    ._1._Two_Sum
+    .Solution
+    .GetMatchingElements_O_n2(twoSumInputElements, twoSumTarget);
+LeetCode_Solutions
+    ._1._Two_Sum
+    .Solution
+    .GetMatchingElements_O_n(twoSumInputElements, twoSumTarget);
+LeetCode_Solutions
+    ._9._Palindrome_Number
+    .Solution
+    .IsPalindromeNumber();
+LeetCode_Solutions
+    ._13._Roman_to_Integer
+    .Solution
+    .GetIntegerFromRoman();
+
+Environment.Exit(0);
